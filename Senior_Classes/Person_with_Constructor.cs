37710c0554fbf0db19114e0b5dbf88c4fd45ba5a
Arktoss <111780704+Arktoss@users.noreply.github.com>
2023-09-18 13:14:52 -0400

    Add project files.

---
+++ Senior_Classes/Person_with_Constructor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Senior_Classes
+{
+    internal class Person_with_Constructor
+    {
+        //Fields
+        private string name;
+        private int age;
+        private double height;
+
+        //Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+
+        //Methods
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello my name is {name}and I am {age} years old. I am {height}feet tall. ")
+        }
+
+        // Constructor
+        public Person_with_Constructor(string initialName, int initialage, double initialheight)
+        {
+            name = initialName;
+            age = initialage;
+            height = initialheight;
+        }
+    }
+}
